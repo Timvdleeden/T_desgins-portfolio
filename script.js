d86aa4b63820e31d0477ac383fc1681aab4e4336
Timvdleeden <58260610+Timvdleeden@users.noreply.github.com>
2020-01-08 09:29:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,16 @@
 	  // Else use ‘day’ theme
 	  document.body.className = "day";
 }
+
+ document.getElementById("sport").onclick = function() {
+     document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+ }
+ document.getElementById("AHkleur").onclick = function() {
+     document.getElementById("AHkleur").innerHTML = "Ik werk al bijna 2 jaar bij Albert Heijn en ik word binnekort misschien teamleider!";
+ }
+ document.getElementById("cmd").onclick = function() {
+     document.getElementById("cmd").innerHTML = "Tot nu toe heb ik alle punten gehaald op CMD, ik heb dus mijn propedeuse!";
+ }
+ document.getElementById("daan").onclick = function() {
+     document.getElementById("daan").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. ";
+ }
