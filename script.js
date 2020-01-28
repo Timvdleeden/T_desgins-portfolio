37f8b54063cbf7cb5667f7a0b2f1b91b71e9b0cc
Timvdleeden <58260610+Timvdleeden@users.noreply.github.com>
2020-01-28 11:48:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,96 @@
-
-	var tijd = new Date().getHours();
-	if (tijd > 18 || tijd < 6) {
-	  // If time is after 6PM or before 6AM, apply night theme to ‘body’
-	  document.body.className = "night";
-    }
-    else {
-	  // Else use ‘day’ theme
-	  document.body.className = "day";
+var tijd = new Date().getHours();
+if (tijd > 18 || tijd < 6) {
+    // wanneer het later dan 6 uur of voor 6 uur (ochtend) is, doe nachtthema.
+    document.body.className = "night";
+} else {
+    // Anders dagthema
+    document.body.className = "day";
 }
 
- document.getElementById("sport").onclick = function() {
-     document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
- }
- document.getElementById("AHkleur").onclick = function() {
-     document.getElementById("AHkleur").innerHTML = "Ik werk al bijna 2 jaar bij Albert Heijn en ik word binnekort misschien teamleider!";
- }
- document.getElementById("cmd").onclick = function() {
-     document.getElementById("cmd").innerHTML = "Tot nu toe heb ik alle punten gehaald op CMD, ik heb dus mijn propedeuse!";
- }
- document.getElementById("daan").onclick = function() {
-     document.getElementById("daan").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. ";
- }
+
+
+
+document.querySelector("button").addEventListener("click", function () {
+    document.querySelector("button").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+})
+
+document.querySelector("button:nth-child(2)").addEventListener("click", function () {
+    document.querySelector("button:nth-child(2)").innerHTML = "Ik werk al bijna 2 jaar bij Albert Heijn en ik word binnekort misschien teamleider!";
+})
+
+document.querySelector("button:nth-child(3)").addEventListener("click", function () {
+    document.querySelector("button:nth-child(3)").innerHTML = "Tot nu toe heb ik alle punten gehaald op CMD, ik heb dus mijn propedeuse!";
+})
+
+document.querySelector("button:nth-child(4)").addEventListener("click", function () {
+    document.querySelector("button:nth-child(4)").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. ";
+})
+
+
+
+
+
+
+//Classes gebruikt omdat het niet lukte
+
+/*
+document.querySelector(".box1").addEventListener("click", function(){
+    document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+})
+
+document.querySelector(".box2").addEventListener("click", function(){
+    document.getElementById("AHkleur").innerHTML = "Ik werk al bijna 2 jaar bij Albert Heijn en ik word binnekort misschien teamleider!";
+})
+
+document.querySelector(".box3").addEventListener("click", function(){
+    document.getElementById("cmd").innerHTML = "Tot nu toe heb ik alle punten gehaald op CMD, ik heb dus mijn propedeuse!";
+} )
+
+document.querySelector(".box4").addEventListener("click", function(){
+    document.getElementById("daan").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. ";
+})
+*/
+
+
+
+
+
+
+//Probeersels 
+
+//document.querySelector("button.box").addEventListener("click", function(){
+//document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+
+//document.querySelector('section button:nth-child(1)').addEventListener("click", function(){
+//document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+
+
+//document.querySelectorAll('section button:nth-of-type(2)').addEventListener("click", function(){
+//document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+
+
+
+
+
+//Code met addevent, maar geen queryselector.
+
+/*document.getElementById("sport").addEventListener("click", function(){
+    document.getElementById("sport").innerHTML = "Ik tafeltennis al bijna 10 jaar op hoog niveau!";
+})
+document.getElementById("AHkleur").addEventListener("click", function () {
+    document.getElementById("AHkleur").innerHTML = "Ik werk al bijna 2 jaar bij Albert Heijn en ik word binnekort misschien teamleider!";
+})
+document.getElementById("cmd").addEventListener("click", function () {
+    document.getElementById("cmd").innerHTML = "Tot nu toe heb ik alle punten gehaald op CMD, ik heb dus mijn propedeuse!";
+} )
+document.getElementById("daan").addEventListener("click", function () {
+    document.getElementById("daan").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. ";
+})
+*/
+
+
+//Eerste code met onclick
+
+/* document.getElementById("daan").onclick = function () {
+    document.getElementById("daan").innerHTML = "Sinds bijna 5 maanden heb ik een hele lieve vriendin genaamd Danielle. "; }
+*/
